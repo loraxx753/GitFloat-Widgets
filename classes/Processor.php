266eb0e4b6f8282fc97fc1296ae10bf0f72fde9a
Kevin Baugh <loraxx753@gmail.com>
2015-09-11 12:05:28 -0500

    added commit_audit as a branch

--- classes/Processor.php
+++ classes/Processor.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * DESCRIPTION OF WHAT EXACTLY YOUR WIDGET DOES 
+ * Commit Audit takes commits from a certain time period and matches them to a regex. 
  * 
  * @package   GitFloat
- * @version   NUMBA
- * @author    YOUR NAME
+ * @version   1.0
+ * @author    Kevin Baugh
  */
 
-namespace AUTHOR\WIDGET_NAME;
+namespace Loraxx753\Commit_Audit;
 
 /**
  * Processes the commit audit request
@@ -15,26 +15,72 @@ namespace AUTHOR\WIDGET_NAME;
 class Processor extends \GitFloat\Base_Processor {
 
 	/**
-	 * Constructs the proccessor.
+	 * Use github and twig
 	 */
 	function __construct() {
-		// Examples 
-		// $this->use_github();
-		// $this->use_twig();
+		$this->use_github();
+		$this->use_twig();
 
 	}
 
 	/**
-	 * Runs the widget and presents results
+	 * Runs the commit audit and presents results
+	 * @param  string  $auditSince  Time frame to pull commits from
+	 * @param  string  $branch      Branch name
+	 * @param  mixed $commitRegex   Default false, regex to compare commits against
+	 * @return string               Twig response
 	 */
-	public function run($arg1, $arg2, $optionalArg = false) {
+	public function run($auditSince, $branch, $commitRegex = false) {
+		$repo = $this->github->api('repo')->commits();
+		$paginator  = new \Github\ResultPager($this->github);
+		$parameters = array($_SESSION['organization'], $_SESSION['repo'], array('sha' => $branch, 'since' => $auditSince));
+		$results    = $paginator->fetchAll($repo, 'all', $parameters);
+
+		foreach ($results as $result) {
+			// If it matches the regex, then bold the match
+			if($commitRegex) {
+				preg_match($commitRegex, $result['commit']['message'], $matches);
+				$result['commit']['message'] = preg_replace($commitRegex, '<b>$1</b>', $result['commit']['message']);				
+			}
+			// If there's no regex OR there's a match, then it's good
+			if(isset($matches[1]) || !$commitRegex) {
+				// If there's no array for this person yet
+				if(!isset($commits['names'][$result['commit']['author']['name']])) {
+					$commits['names'][$result['commit']['author']['name']]['good'] = array();
+					$commits['names'][$result['commit']['author']['name']]['bad'] = array();
+				}
+				$commits['names'][$result['commit']['author']['name']]['good'][] = $this->parse_commit($result);
+				$commits['good'][] = $this->parse_commit($result);
+			}
+			// Else it's bad
+			else {
+				// If there's no array for this person yet
+				if(!isset($commits['names'][$result['commit']['author']['name']])) {
+					$commits['names'][$result['commit']['author']['name']]['good'] = array();
+					$commits['names'][$result['commit']['author']['name']]['bad'] = array();
+				}
+				$commits['names'][$result['commit']['author']['name']]['bad'][] = $this->parse_commit($result);
+				$commits['bad'][] =  $this->parse_commit($result);
+			}
+		} 
 
 		return $this->twig->render('output.twig', 
-							array(
-								'argument1'        => $arg1,
-								'argument2'        => $arg2,
-								'optionalArgument' => $optionalArg
-								));
+							array('commits' => $commits));
 	}
 
+	/**
+	 * Makes the commits pretty
+	 * @param  array   $commit The contents of the commit
+	 * @return string          Twig response
+	 */
+	private function parse_commit($commit) {
+		$avatar = (isset($commit['author']['avatar_url'])) ? $commit['author']['avatar_url'] : "/assets/img/placeholder.jpg";
+		$heading = $commit['commit']['author']['name']." - ".date("m-d-Y @ h:i a", strtotime($commit['commit']['author']['date']));
+		$content = "<p>".$commit['commit']['message']."</p>";
+		
+		return $this->twig->render('bootstrap/media_object.twig', 
+							array('image'  => $avatar, 
+								  'heading' => $heading,
+								  'content' => $content));
+	}
 }
--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-// $.widget("ID_OF_WIDGET", [ARGUMENTS_THAT_MATCH_PROCESSOR->run()], CALLBACK_FUNCTION);
-$.widget("example", ['arg1', 'arg2', 'optionalArg'], function(data) {
-	alert("This happened after execution!");
+$.widget("commit_audit", ['auditSince', 'auditSinceBranch', 'commitRegex'], function() {
+	$(".totalsHeader").on('click', function(e) {
+		e.preventDefault();
+		$(".details").slideToggle();
+	});
+	$(".details li .target-area").on("click", function(e) {
+		e.preventDefault();
+		$(this).parent().find(".individual-results").slideToggle();
+	});
+});
+
+// Load the regex if it's been saved
+if(localStorage.commitRegex) {
+	$("#commitRegex").val(localStorage.commitRegex);
+}
+// Trigger a popup if they save the regex
+$(".commit-audit-save-regex").popover({
+	trigger : 'manual',
+	content : 'Saved!',
+	delay   : { 'show' : 0, 'hide' : 500} 
+});
+
+// Save the regex to localStorage
+$(".commit-audit-save-regex").on('click', function(e) {
+	$this = $(this);
+	localStorage.setItem('commitRegex', $("#commitRegex").val());
+	$this.popover('show');
+	setTimeout(function(){ $this.popover('hide'); }, 800);
 });
