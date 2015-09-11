fdaf693d087f4da648bcb92446a1ae669693cb6f
Kevin Baugh <loraxx753@gmail.com>
2015-09-11 12:08:36 -0500

    added compare_branch ...branch

--- classes/Processor.php
+++ classes/Processor.php
@@ -1,40 +1,60 @@
 <?php
 /**
- * DESCRIPTION OF WHAT EXACTLY YOUR WIDGET DOES 
+ * Compare branch takes two branches and finds commits on one that aren't on another. 
  * 
  * @package   GitFloat
- * @version   NUMBA
- * @author    YOUR NAME
+ * @version   1.0
+ * @author    Kevin Baugh
  */
 
-namespace AUTHOR\WIDGET_NAME;
+namespace Loraxx753\Compare_Branch;
 
 /**
- * Processes the commit audit request
+ * Processes the compare branch request
  */
 class Processor extends \GitFloat\Base_Processor {
 
-	/**
-	 * Constructs the proccessor.
-	 */
 	function __construct() {
-		// Examples 
-		// $this->use_github();
-		// $this->use_twig();
+		$this->use_github();
+		$this->use_twig();
 
 	}
 
 	/**
-	 * Runs the widget and presents results
+	 * Rungs the branch compare between two branches
+	 * @param  string $compareCommitsFrom Branch with latest information
+	 * @param  string $compareCommitsTo   Branch with NOT lastest information
+	 * @return string                     Twig response
 	 */
-	public function run($arg1, $arg2, $optionalArg = false) {
+	public function run($compareCommitsFrom = 'dev', $compareCommitsTo = 'master') {
 
-		return $this->twig->render('output.twig', 
-							array(
-								'argument1'        => $arg1,
-								'argument2'        => $arg2,
-								'optionalArgument' => $optionalArg
-								));
+		$result = $this->github->api('repos')->commits()->compare($_SESSION['organization'], $_SESSION['repo'], $compareCommitsTo, $compareCommitsFrom);
+		$commits = array();
+		foreach ($result['commits'] as $commit) {
+			$commits[] = $this->parse_commit($commit);
+		}
+		// Have to reverse so it's latest first.
+		array_reverse($commits);
+		$result['commits'] = $commits;
+
+		return $this->twig->render('output.twig',
+							array('result' => $result,
+								'compareCommitsFrom' => $compareCommitsFrom,
+								'compareCommitsTo' => $compareCommitsTo));
 	}
-
+	/**
+	 * Makes the commits pretty
+	 * @param  array   $commit The contents of the commit
+	 * @return string          Twig response
+	 */
+	private function parse_commit($commit) {
+		$avatar = (isset($commit['author']['avatar_url'])) ? $commit['author']['avatar_url'] : "/assets/img/placeholder.jpg";
+		$heading = $commit['commit']['author']['name']." - ".date("m-d-Y @ h:i a", strtotime($commit['commit']['author']['date']));
+		$content = "<p>".$commit['commit']['message']."</p>";
+		
+		return $this->twig->render('bootstrap/media_object.twig', 
+							array('image'  => $avatar, 
+								  'heading' => $heading,
+								  'content' => $content));
+	}
 }
--- js/script.js
+++ js/script.js
@@ -1,4 +1,1 @@
-// $.widget("ID_OF_WIDGET", [ARGUMENTS_THAT_MATCH_PROCESSOR->run()], CALLBACK_FUNCTION);
-$.widget("example", ['arg1', 'arg2', 'optionalArg'], function(data) {
-	alert("This happened after execution!");
-});
+$.widget('compare_branch', ['compareCommitsFrom', 'compareCommitsTo']);
